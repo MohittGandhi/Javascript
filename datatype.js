257c6e1c3696d003250bfc9a01bba20dfa8fbb87
mohit_s <mohit.gandhi@supersourcing.com>
2024-12-24 18:25:23 +0530

    first commit

---
+++ datatype.js
@@ -0,0 +1,12 @@
+ // treat all JS Code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(3+3); //code readablitiy should be high
+
+let age = 90
+let name = "mohit"
+
+console.log([typeof age, typeof name]);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountID = 45
+let accountemail = "mohit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+
+//accountID = 90;
+
+console.log(accountID);
+// Prefer not to use var because of  issue inn block  scope and functional scope
+
+accountemail = "mohit123@gmail.com"
+accountPassword = "543212"
+accountCity = "Pune"
+
+console.log([accountID,accountPassword,accountemail,accountCity])
