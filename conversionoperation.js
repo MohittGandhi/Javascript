e14fd77f08a52623deeaf347cffee5d139b2f070
mohit_s <mohit.gandhi@supersourcing.com>
2024-12-24 18:49:10 +0530

    operations in java

---
+++ conversionoperation.js
@@ -0,0 +1,18 @@
+let score = "32ber"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+//1 => true 0 =>  false
+//"" => false
+
+let somenumber  = 33
+let stringnumber = String(somenumber);
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
